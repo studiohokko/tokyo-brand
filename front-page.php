a712e846f3ecc7369f83af7803d30cd991d04956
hokko <studiohokko@gmail.com>
2026-06-12 09:32:50 +0900

    choice実装

--- front-page.php
+++ front-page.php
@@ -8,8 +8,9 @@
    <section class="p-fv">
       <div class="p-fv__inner">
          <div class="p-fv__heading">
-            <h1 class="p-fv__catch">東京<span class="-small">で</span><span class="-spacingSmall">ブラ<span>ン</span>ド</span>品<span class="-small">を</span><br>高く売る<span class="-small">なら</span><br
-                  class="u-only__sp">東京ぶらんど</h1>
+            <h1 class="p-fv__catch">東京<span class="-small">で</span><span
+                  class="-spacingSmall">ブラ<span>ン</span>ド</span>品<span class="-small">を</span><br>高く売る<span
+                  class="-small">なら</span><br class="u-only__sp">東京ぶらんど</h1>
          </div>
          <!-- /.p-fv__heading -->
          <div class="p-fv__content">
@@ -43,7 +44,31 @@
    </section>
    <!-- /.p-fv -->
 
-
+   <!-- choice   ///////////////////////////////////////////////////// -->
+   <div class="p-choice">
+      <p class="p-choice__pop">選べる<span class="-color">2つ</span><span class="-small">の</span>買取方法</p>
+      <ul class="p-choice__methods">
+         <li class="p-choice__method">
+            <a href="" class="p-choice__methodLink -store">
+               <figure class="p-choice__methodImage">
+                  <img src='<?php echo esc_url(get_theme_file_uri('dev/public/assets/img/icon_building.svg')); ?>'
+                     alt='' width='' height='' loading='lazy'>
+               </figure>
+               <p class="p-choice__methodText">店頭買取</p>
+            </a>
+         </li>
+         <li class="p-choice__method">
+            <a href="" class="p-choice__methodLink -delivery">
+               <figure class="p-choice__methodImage">
+                  <img src='<?php echo esc_url(get_theme_file_uri('dev/public/assets/img/icon_car.svg')); ?>' alt=''
+                     width='' height='' loading='lazy'>
+               </figure>
+               <p class="p-choice__methodText">宅配買取</p>
+            </a>
+         </li>
+      </ul>
+   </div>
+   <!-- /.p-choice -->
 
 </main>
 
